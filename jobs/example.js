3d900f72ac90e1d19c210e21b0667782bcb1634e
bengreenier <ben@bengreenier.com>
2015-04-15 08:33:56 -0400

    updating job example

--- jobs/example.js
+++ jobs/example.js
@@ -4,11 +4,11 @@ module.exports = function(opts) {
 	
 	// get a cat pic
 	request.get("https://farm7.staticflickr.com/6100/6303228181_59371c29dc_q_d.jpg", function(e, r, b) {
-		if (e || r.statusCode != 200) throw new Error("failed to get image");
+		if (e || r.statusCode != 200) return opts.error("failed to get image");
 
 		// upload it to s3
 		opts.s3.upload({Bucket: opts.bucket, Body: b, Key: uuid.v4()+".jpg"}, function(e) {
-			if (e) opts.error(e); //for handling of async errors
+			if (e) return opts.error(e); //for handling of async errors
 		});
 	});
 };
